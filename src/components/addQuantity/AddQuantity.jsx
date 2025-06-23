ad0b29978c930ad11e3b5ac15e3dd9a3ad1dd269
aadeyetwhya <adityakafle@gmail.com>
2025-06-23 19:28:40 +0545

    dynamic for tailwind padding

--- src/components/addQuantity/AddQuantity.jsx
+++ src/components/addQuantity/AddQuantity.jsx
@@ -1,26 +1,26 @@
 import React, { useState } from 'react'
 
-function AddQuantity() {
-    const [quantity,setQuantify]=useState(1);
+function AddQuantity({title='Quantity',pxaxis=3,pyaxis=1}) {
+  const [quantity, setQuantify] = useState(1);
   return (
     <div className='flex items-center text-lg lg:text-xl'>
-        <div className="title">
-            Quantity : 
+      <div className="title">
+        {title}
+      </div>
+      <div className="quantity flex ml-2 text-white ">
+        <div className={`sub bg-red-700 py-0 px-${pxaxis} flex items-center text-2xl cursor-pointer rounded`}
+          onClick={() => setQuantify((prev) => prev > 1 ? prev - 1 : prev)}>
+          -
         </div>
-        <div className="quantity flex ml-2 text-white ">
-            <div className="sub bg-red-700 py-0 px-3 flex items-center text-2xl cursor-pointer"
-            onClick={()=>setQuantify((prev)=>prev>1?prev-1:prev)}>
-                 -
-            </div>
-            <div className="showQuantity bg-gray-300 py-0 px-3 flex ml-3  mr-3 items-center text-black">
-       {quantity}
-            </div>
-            <div className="add bg-red-700 py-0 lg:py-1 px-3 flex items-center cursor-pointer"
-                        onClick={()=>setQuantify((prev)=>prev<5?prev+1:prev)}>
+        <div className={`showQuantity bg-gray-300 py-0 px-${pxaxis} flex ml-3  mr-3 items-center text-black`}>
+          {quantity}
+        </div>
+        <div className={`add bg-red-700 py-0 lg:py-${pyaxis} px-${pxaxis} flex items-center cursor-pointer rounded`}
+          onClick={() => setQuantify((prev) => prev < 5 ? prev + 1 : prev)}>
 
-                +
-            </div>
+          +
         </div>
+      </div>
     </div>
   )
 }
